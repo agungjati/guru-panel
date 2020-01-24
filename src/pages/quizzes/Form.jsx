91e31aa9c1f1f6a6be78e8c4bdc37f59f9440c62
agungjati <agungjati94@gmail.com>
2020-01-24 11:11:41 +0700

    add realate to question in quizzes

--- src/pages/quizzes/Form.jsx
+++ src/pages/quizzes/Form.jsx
@@ -6,6 +6,7 @@ import QuizController from '../../controllers/quizzes'
 import ClassController from '../../controllers/classes'
 import ChapterController from '../../controllers/chapters'
 import CourseController from '../../controllers/courses'
+import QuestionController from '../../controllers/questions'
 import { QuizModel } from '../../model/QuizModel'
 import 'toastr/build/toastr.min.css'
 import toastr from 'toastr'
@@ -17,6 +18,7 @@ class Form extends Component {
   classController = new ClassController();
   chapterController = new ChapterController();
   courseController = new CourseController();
+  questionController = new QuestionController();
 
   state = {
     model: { ...QuizModel },
@@ -52,19 +54,29 @@ class Form extends Component {
     this.setState({ model: { ...this.state.model, [type]: value } })
   }
 
-  onSaveForm = () => {
-    const chapters = this.state.model.chapters.map(x => x.value);
-    const courses = this.state.model.courses.map(x => x.value);
-    const classes = this.state.model.classes.map(x => x.value);
+  isNotValidInputForm = () => {
+    const { model } = this.state;
+    const fieldRequired = [model.quizName, model.description, model.capacity, model.durationMinute, model.totalQuestions];
+    return fieldRequired.includes("") || fieldRequired.includes(null) || fieldRequired.includes(undefined);
+  }
 
-    if (this.state.isEntry) {
-      this.quizController.onInsert({ ...this.state.model, chapters, courses, classes })
-        .then(() => this.toastr.success('Successfully saved'))
-        .catch(e => this.toastr.error(e.message))
-    } else {
-      this.quizController.onUpdate({ ...this.state.model, chapters, courses, classes })
-        .then(() => this.toastr.success('Successfully saved'))
-        .catch(e => this.toastr.error(e.message))
+  onSaveForm = () => {
+    if (this.isNotValidInputForm()) {
+      this.toastr.warning('The field required must be filled.')
+    }else{
+      const chapters = this.state.model.chapters.map(x => x.value);
+      const courses = this.state.model.courses.map(x => x.value);
+      const classes = this.state.model.classes.map(x => x.value);
+
+      if (this.state.isEntry) {
+        this.quizController.onInsert({ ...this.state.model, chapters, courses, classes })
+          .then(() => this.toastr.success('Successfully saved'))
+          .catch(e => this.toastr.error(e.message))
+      } else {
+        this.quizController.onUpdate({ ...this.state.model, chapters, courses, classes })
+          .then(() => this.toastr.success('Successfully saved'))
+          .catch(e => this.toastr.error(e.message))
+      }
     }
   }
 
@@ -121,6 +133,27 @@ class Form extends Component {
     })
   }
 
+  loadQuestion = (inputValue, callback) => {
+    this.questionController
+      .getList({ _q: inputValue })
+      .then(res => res.data)
+      .then(res => {
+        const questions = res.map(x => ({ value: x.id, label: x.question }))
+        callback([...questions])
+      })
+
+  }
+
+  handleChangeQuestion = (question) => {
+    if (question === null) { question = [] }
+    this.setState({
+      model: {
+        ...this.state.model,
+        questions: [...question]
+      }
+    })
+  }
+
 
   loadCourse = (inputValue, callback) => {
     if (inputValue) {
@@ -165,129 +198,134 @@ class Form extends Component {
           </div>
         </div>
         <div className="content">
-          
-              <div className='row'>
-                <div className='col-md-8'>
-                  <div className="card">
-                    <div className="card-body pad">
+          <div className='row'>
+            <div className='col-md-8'>
+              <div className="card">
+                <div className="card-body pad">
+                  <div className="form-group">
+                    <label htmlFor="inputQuizName">Quiz Name</label>
+                    <input
+                      type="text"
+                      id="inputQuizName"
+                      className="form-control"
+                      value={model.quizName}
+                      onChange={(ev) => this.onChangeModel("quizName", ev.target.value)}
+                    />
+                  </div>
+                  <CKEditor
+                    onBeforeLoad={(CKEDITOR) => (CKEDITOR.disableAutoInline = true)}
+                    config={this.configCKEditor}
+                    data={model.description}
+                    onChange={(ev) => this.onChangeModel("description", ev.editor.getData())} />
+                  <div className="row">
+                    <div className="col-md-4">
                       <div className="form-group">
-                        <label htmlFor="inputQuizName">Quiz Name</label>
+                        <label htmlFor="inputCapacity">Capacity</label>
                         <input
-                          type="text"
-                          id="inputQuizName"
+                          type="number"
+                          id="inputCapacity"
                           className="form-control"
-                          value={model.quizName}
-                          onChange={(ev) => this.onChangeModel("quizName", ev.target.value)}
-                        />
-                      </div>
-                      <CKEditor
-                        onBeforeLoad={(CKEDITOR) => (CKEDITOR.disableAutoInline = true)}
-                        config={this.configCKEditor}
-                        data={model.description}
-                        onChange={(ev) => this.onChangeModel("description", ev.editor.getData())} />
-                      <div className="row">
-                        <div className="col-md-4">
-                          <div className="form-group">
-                            <label htmlFor="inputCapacity">Capacity</label>
-                            <input
-                              type="number"
-                              id="inputCapacity"
-                              className="form-control"
-                              value={model.capacity}
-                              onChange={(ev) => this.onChangeModel("capacity", ev.target.value)} />
-                          </div>
-                        </div>
-                        <div className="col-md-4">
-                          <div className="form-group">
-                            <label htmlFor="inputDurationMinute">Duration Minute</label>
-                            <input
-                              type="number"
-                              id="inputDurationMinute"
-                              className="form-control"
-                              value={model.durationMinute}
-                              onChange={(ev) => this.onChangeModel("durationMinute", ev.target.value)} />
-                          </div>
-                        </div>
-                        <div className="col-md-4">
-                          <div className="form-group">
-                            <label htmlFor="inputTotalQuestions">Total Questions</label>
-                            <input
-                              type="number"
-                              id="inputTotalQuestions"
-                              className="form-control"
-                              value={model.totalQuestions}
-                              onChange={(ev) => this.onChangeModel("totalQuestions", ev.target.value)} />
-                          </div>
-                        </div>
+                          value={model.capacity}
+                          onChange={(ev) => this.onChangeModel("capacity", ev.target.value)} />
                       </div>
                     </div>
-                  </div>
-                </div>
-                <div className='col-md-4'>
-                  <div className="card">
-                    <div className="card-body pad">
-                      <div className="row">
-                        <div className="col-md-12">
-                          <div className="form-group">
-                            <label>Question Detail</label>
-                            <AsyncSelect
-                              placeholder="Select a chapter"
-                              closeMenuOnSelect={false}
-                              isMulti
-                              cacheOptions
-                              value={model.chapters}
-                              loadOptions={this.loadChapter}
-                              onChange={this.handleChangeChapter} />
-                          </div>
-                        </div>
+                    <div className="col-md-4">
+                      <div className="form-group">
+                        <label htmlFor="inputDurationMinute">Duration Minute</label>
+                        <input
+                          type="number"
+                          id="inputDurationMinute"
+                          className="form-control"
+                          value={model.durationMinute}
+                          onChange={(ev) => this.onChangeModel("durationMinute", ev.target.value)} />
                       </div>
-                      <div className="row">
-                        <div className="col-md-12">
-                          <div className="form-group">
-                            <label>Question Detail</label>
-                            <AsyncSelect
-                              placeholder="Select a class"
-                              closeMenuOnSelect={false}
-                              isMulti
-                              cacheOptions
-                              value={model.classes}
-                              loadOptions={this.loadClass}
-                              onChange={this.handleChangeClass} />
-                            {model.classes.map((cls, idx) =>
-                              <div key={idx} className="d-flex justify-content-between align-items-center mt-1 shadow-sm p-1">
-                                <span>{cls.label}</span>
-                                <a href="#"
-                                  onClick={() => this.deleteClass(cls.id)} className="btn btn-sm btn-danger">x</a>
-                              </div>)}
-                          </div>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="col-md-12">
-                          <div className="form-group">
-                            <label>Question Detail</label>
-                            <AsyncSelect
-                              placeholder="Select a course"
-                              closeMenuOnSelect={false}
-                              isMulti
-                              cacheOptions
-                              value={model.courses}
-                              loadOptions={this.loadCourse}
-                              onChange={this.handleCourseClass} />
-                            {model.courses.map((cls, idx) =>
-                              <div key={idx} className="d-flex justify-content-between align-items-center mt-1 shadow-sm p-1">
-                                <span>{cls.label}</span>
-                                <a href="#"
-                                  onClick={() => this.deleteCourse(cls.id)} className="btn btn-sm btn-danger">x</a>
-                              </div>)}
-                          </div>
-                        </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="form-group">
+                        <label htmlFor="inputTotalQuestions">Total Questions</label>
+                        <input
+                          type="number"
+                          id="inputTotalQuestions"
+                          className="form-control"
+                          value={model.totalQuestions}
+                          onChange={(ev) => this.onChangeModel("totalQuestions", ev.target.value)} />
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            
+            </div>
+            <div className='col-md-4'>
+              <div className="card">
+                <div className="card-body pad">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <div className="form-group">
+                        <label>Chapter</label>
+                        <AsyncSelect
+                          placeholder="Select a chapter"
+                          closeMenuOnSelect={false}
+                          isMulti
+                          cacheOptions
+                          value={model.chapters}
+                          loadOptions={this.loadChapter}
+                          onChange={this.handleChangeChapter} />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-12">
+                      <div className="form-group">
+                        <label>Class</label>
+                        <AsyncSelect
+                          placeholder="Select a class"
+                          closeMenuOnSelect={false}
+                          isMulti
+                          cacheOptions
+                          value={model.classes}
+                          loadOptions={this.loadClass}
+                          onChange={this.handleChangeClass} />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-12">
+                      <div className="form-group">
+                        <label>Questions</label>
+                        <AsyncSelect
+                          isMulti
+                          placeholder="Select questions"
+                          closeMenuOnSelect={false}
+                          cacheOptions
+                          value={model.questions}
+                          loadOptions={this.loadQuestion}
+                          onChange={this.handleChangeQuestion}
+                        />
+
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="row">
+                    <div className="col-md-12">
+                      <div className="form-group">
+                        <label>Course</label>
+                        <AsyncSelect
+                          placeholder="Select a course"
+                          closeMenuOnSelect={false}
+                          isMulti
+                          cacheOptions
+                          value={model.courses}
+                          loadOptions={this.loadCourse}
+                          onChange={this.handleCourseClass} />
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
